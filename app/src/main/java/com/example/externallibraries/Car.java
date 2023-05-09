5e1f28173c4c1e73b8f1438c786c424fea3826ad
Carl Gösfeldt <c22cargo@student.his.se>
2023-05-09 23:40:08 +0200

    Added a "Car" class.

---
+++ app/src/main/java/com/example/externallibraries/Car.java
@@ -0,0 +1,17 @@
+package com.example.externallibraries;
+
+public class Car {
+
+    private int weight;
+    private int horsepower;
+
+    private String color;
+    private String brand;
+
+    public Car(int weight, int horsepower, String color, String brand) {
+        this.weight = weight;
+        this.horsepower = horsepower;
+        this.color = color;
+        this.brand = brand;
+    }
+}
